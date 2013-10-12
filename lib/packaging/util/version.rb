e1dd740fbcd009cc8375274c9e2676a75acf8425
Moses Mendoza <moses@puppetlabs.com>
2013-10-12 16:01:30 -0600

    Initial work on rpm

--- lib/packaging/util/version.rb
+++ lib/packaging/util/version.rb
@@ -182,5 +182,20 @@ module Pkg::Util::Version
       return TRUE if get_dash_version =~ /^\d+\.\d+\.\d+-*rc\d+/
       FALSE
     end
+
+    # Utility method to return the dist method if this is a redhat box. We use this
+    # in rpm packaging to define a dist macro, and we use it in the pl:fetch task
+    # to disable ssl checking for redhat 5 because it has a certs bundle so old by
+    # default that it's useless for our purposes.
+    def el_version()
+      rpm = Pkg::Util::Tool.find_tool('rpm', :required => true)
+      if File.exists?('/etc/fedora-release')
+        nil
+      elsif File.exists?('/etc/redhat-release')
+        return %x{#{rpm} -q --qf \"%{VERSION}\" $(#{rpm} -q --whatprovides /etc/redhat-release )}
+      end
+    end
+
+
   end
 end

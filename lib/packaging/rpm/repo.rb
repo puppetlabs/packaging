f1c120266ee33c978cd233033ef467365a9c4c9a
Matthaus Owens <matthaus@puppetlabs.com>
2014-07-31 16:47:49 -0700

    (CPR-56) Migrate rpm repo tasks into Pkg::Rpm::Repo
    This commit moves the current tasks in tasks/rpm_repos.rake into the
    Pkg::Rpm::Repo module. The old tasks now simply forward the request on
    to the equivalent method call. This commit also includes a minimum of
    refactoring.

--- lib/packaging/rpm/repo.rb
+++ lib/packaging/rpm/repo.rb
@@ -1,25 +1,154 @@
 # Utilities for working with rpm repos
+require 'fileutils'
 
 module Pkg::Rpm::Repo
   class << self
+    def base_url
+      "http://#{Pkg::Config.builds_server}/#{Pkg::Config.project}/#{Pkg::Config.ref}"
+    end
 
-    def ship_repo_configs
-      Pkg::Util::File.empty_dir?("pkg/repo_configs/rpm") and fail "No repo configs have been generated! Try pl:rpm_repo_configs."
+    def ship_repo_configs(target = "repo_configs")
+      Pkg::Util::File.empty_dir?("pkg/#{target}/rpm") and fail "No repo configs have been generated! Try pl:rpm_repo_configs."
       invoke_task("pl:fetch")
-      repo_dir = "#{Pkg::Config.jenkins_repo_path}/#{Pkg::Config.project}/#{Pkg::Config.ref}/repo_configs/rpm"
+      repo_dir = "#{Pkg::Config.jenkins_repo_path}/#{Pkg::Config.project}/#{Pkg::Config.ref}/#{target}/rpm"
       Pkg::Util::Net.remote_ssh_cmd(Pkg::Config.distribution_server, "mkdir -p #{repo_dir}")
       retry_on_fail(:times => 3) do
-        Pkg::Util::Net.rsync_to("pkg/repo_configs/rpm/", Pkg::Config.distribution_server, repo_dir)
+        Pkg::Util::Net.rsync_to("pkg/#{target}/rpm/", Pkg::Config.distribution_server, repo_dir)
       end
+    end
+
+    def repo_creation_command(createrepo)
+      cmd = 'for repodir in $(find ./ -name "*.rpm" | xargs -I {} dirname {}) ; do '
+      cmd << "[ -d ${repodir} ] || continue; "
+      cmd << "pushd ${repodir} && #{createrepo} --checksum=sha --database --update . ; popd ; "
+      cmd << "done "
     end
 
     def create_repos(directory = "repos")
       Dir.chdir(directory) do
-        createrepo = Pkg::Util::Tool.find_tool('createrepo')
-        cmd = 'for repodir in $(find ./ -name "*.rpm" | xargs -I {} dirname {}) ; do '
+        createrepo = Pkg::Util::Tool.check_tool('createrepo')
+        Pkg::Util::Execution.ex("bash -c '#{repo_creation_command(createrepo)}'")
+      end
+    end
+
+    def retrieve_repo_configs(target = "repo_configs")
+      wget = Pkg::Util::Tool.check_tool("wget")
+      FileUtils.mkdir_p("pkg/#{target}")
+      config_url = "#{base_url}/#{target}/rpm/"
+      begin
+        Pkg::Util::Execution.ex("#{wget} -r -np -nH --cut-dirs 3 -P pkg/#{target} --reject 'index*' #{config_url}")
+      rescue
+        fail "Couldn't retrieve rpm yum repo configs. See preceding http response for more info."
+      end
+    end
+
+    def generate_repo_configs(source = "repos", target = "repo_configs")
+      # We have a hard requirement on wget because of all the download magicks
+      # we have to do
+      #
+      wget = Pkg::Util::Tool.check_tool("wget")
+
+      # This is the standard path to all build artifacts on the distribution
+      # server for this commit
+      #
+      repo_base = "#{base_url}/#{source}/"
+
+      # First check if the artifacts directory exists
+      #
+
+      # We have to do two checks here - first that there are directories with
+      # repodata folders in them, and second that those same directories also
+      # contain rpms
+      #
+      repo_urls = Pkg::Util::Execution.ex("#{wget} --spider -r -l 5 --no-parent #{repo_base} 2>&1").split.uniq.reject{ |x| x =~ /\?|index/ }.select{ |x| x =~ /http:.*repodata\/$/ }
+
+      # RPMs will always exist at the same directory level as the repodata
+      # folder, which means if we go up a level we should find rpms
+      #
+      yum_repos = []
+      repo_urls.map{ |x| x.chomp('repodata/') }.each do |url|
+        unless Pkg::Util::Execution.ex("#{wget} --spider -r -l 1 --no-parent #{url} 2>&1").split.uniq.reject{ |x| x =~ /\?|index/ }.select{ |x| x =~ /http:.*\.rpm$/ }.empty?
+          yum_repos << url
+        end
+      end
+
+      yum_repos.empty? and fail "No rpm repos were found to generate configs from!"
+
+      FileUtils.mkdir_p(File.join("pkg", target, "rpm"))
+
+      # Parse the rpm configs file to generate repository configs. Each line in
+      # the rpm_configs file corresponds with a repo directory on the
+      # distribution server.
+      #
+      yum_repos.each do |url|
+        # We ship a base 'srpm' that gets turned into a repo, but we want to
+        # ignore this one because its an extra
+        next if url == "#{base_url}srpm/"
+
+        dist, version, _subdir, arch = url.split('/')[-4..-1]
+
+        # Create an array of lines that will become our yum config
+        #
+        config = ["[pl-#{Pkg::Config.project}-#{Pkg::Config.ref}]"]
+        config << ["name=PL Repo for #{Pkg::Config.project} at commit #{Pkg::Config.ref}"]
+        config << ["baseurl=#{url}"]
+        config << ["enabled=1"]
+        config << ["gpgcheck=0"]
+
+        # Write the new config to a file under our repo configs dir
+        #
+        config_file = File.join("pkg", target, "rpm", "pl-#{Pkg::Config.project}-#{Pkg::Config.ref}-#{dist}-#{version}-#{arch}.repo")
+        File.open(config_file, 'w') { |f| f.puts config }
+      end
+      puts "Wrote yum configuration files for #{Pkg::Config.project} at #{Pkg::Config.ref} to pkg/#{target}/rpm"
+    end
+
+    # Generate yum configuration files that point to the repositories created
+    # on the distribution server with packages created from the current source
+    # repo commit. There is one for each dist/version that is packaged (e.g.
+    # el5, el6, etc). Files are created in pkg/repo_configs/rpm and are named
+    # pl-$project-$sha.conf, and can be placed in /etc/yum.repos.d to enable
+    # clients to install these packages.
+    #
+    def create_repos_from_artifacts
+      begin
+        # Formulate our command string, which will just find directories with rpms
+        # and create and update repositories.
+        #
+        artifact_directory = File.join(Pkg::Config.jenkins_repo_path, Pkg::Config.project, Pkg::Config.ref)
+
+        ##
+        # Test that the artifacts directory exists on the distribution server.
+        # This will give us some more helpful output.
+        #
+        cmd = 'echo "Checking for build artifacts. Will exit if not found." ; '
+        cmd << "[ -d #{artifact_directory}/artifacts ] || exit 1 ; "
+
+        ##
+        # Enter the directory containing the build artifacts and create repos.
+        #
+        cmd << "pushd #{artifact_directory} ; "
+        cmd << 'echo "Checking for running repo creation. Will wait if detected." ; '
+        cmd << "while [ -f .lock ] ; do sleep 1 ; echo -n '.' ; done ; "
+        cmd << 'echo "Setting lock" ; '
+        cmd << "touch .lock ; "
+        cmd << "rsync -avxl artifacts/ repos/ ; pushd repos ; "
+        cmd << "createrepo=$(which createrepo) ; "
+        cmd << 'for repodir in $(find ./ -name "*.rpm" | xargs -I {} dirname {}) ; do '
         cmd << "[ -d ${repodir} ] || continue; "
-        cmd << "pushd ${repodir} && #{createrepo} --checksum=sha --database --update . ; popd ; "
+        cmd << "pushd ${repodir} && ${createrepo} --checksum=sha --database --update . ; popd ; "
         cmd << "done ; popd "
+
+        Pkg::Util::Net.remote_ssh_cmd(Pkg::Config.distribution_server, cmd)
+        # Now that we've created our repositories, we can create the configs for
+        # them
+        Pkg::Rpm::Repo.generate_repo_configs
+
+        # And once they're created, we can ship them
+        Pkg::Rpm::Repo.ship_repo_configs
+      ensure
+        # Always remove the lock file, even if we've failed
+        Pkg::Util::Net.remote_ssh_cmd(Pkg::Config.distribution_server, "rm -f #{artifact_directory}/.lock")
       end
     end
   end
---
+++ spec/lib/packaging/rpm/repo_spec.rb
@@ -0,0 +1,119 @@
+require 'spec_helper'
+
+describe "Pkg::Rpm::Repo" do
+  let(:wget)          { "/opt/tools/bin/wget" }
+  let(:builds_server) { "saturn.puppetlabs.net" }
+  let(:project)       { "rpm_repos" }
+  let(:ref)           { "1234abcd" }
+  let(:base_url)      { "http://#{builds_server}/#{project}/#{ref}" }
+  let(:mocks)         { ["el-5-i386", "el-5-x86_64", "el-5-SRPMS"] }
+  let(:wget_results)  {
+                        mocks.map do |mock|
+                          dist, version, arch = mock.split('-')
+                          "http://#{builds_server}/#{project}/#{ref}/repos/#{dist}/#{version}/products/#{arch}/repodata/"
+                        end.join("\n")
+                      }
+  let(:wget_garbage)  { "\nother things\n and an index\nhttp://somethingelse.com" }
+  let(:repo_configs)  { mocks.map { |mock| "pkg/repo_configs/rpm/pl-#{project}-#{ref}-#{mock}.repo" } }
+
+  # Setup and tear down for the tests
+  around do |example|
+    orig_server = Pkg::Config.builds_server
+    orig_host = Pkg::Config.distribution_server
+    orig_project = Pkg::Config.project
+    orig_ref = Pkg::Config.ref
+    orig_repo_path = Pkg::Config.jenkins_repo_path
+    Pkg::Config.builds_server = builds_server
+    Pkg::Config.project = project
+    Pkg::Config.ref = ref
+    example.run
+    Pkg::Config.builds_server = orig_server
+    Pkg::Config.distribution_server = orig_host
+    Pkg::Config.project = orig_project
+    Pkg::Config.ref = orig_ref
+    Pkg::Config.jenkins_repo_path = orig_repo_path
+  end
+
+  describe "#generate_repo_configs" do
+    it "fails if wget isn't available" do
+      Pkg::Util::Tool.stub(:find_tool).with("wget", {:required => true}) {false}
+      expect {Pkg::Rpm::Repo.generate_repo_configs}.to raise_error(RuntimeError)
+    end
+
+    it "fails if there are no rpm repos available for the build" do
+      Pkg::Util::Tool.should_receive(:find_tool).with("wget", {:required => true}).and_return(wget)
+      Pkg::Util::Execution.should_receive(:ex).with("#{wget} --spider -r -l 5 --no-parent #{base_url}/repos/ 2>&1").and_return("")
+      expect {Pkg::Rpm::Repo.generate_repo_configs}.to raise_error(RuntimeError, /No rpm repos/)
+    end
+
+    it "writes the expected repo configs to disk" do
+      Pkg::Util::Tool.should_receive(:find_tool).with("wget", {:required => true}).and_return(wget)
+      Pkg::Util::Execution.should_receive(:ex).with("#{wget} --spider -r -l 5 --no-parent #{base_url}/repos/ 2>&1").and_return(wget_results + wget_garbage)
+      wget_results.split.each do |result|
+        cur_result = result.chomp('repodata/')
+        Pkg::Util::Execution.should_receive(:ex).with("#{wget} --spider -r -l 1 --no-parent #{cur_result} 2>&1").and_return("#{cur_result}/thing.rpm")
+      end
+      FileUtils.should_receive(:mkdir_p).with("pkg/repo_configs/rpm")
+      config = []
+      repo_configs.each_with_index do |repo_config, i|
+        config[i] = double(File)
+        File.should_receive(:open).with(repo_config, 'w').and_yield(config[i])
+        config[i].should_receive(:puts)
+      end
+      Pkg::Rpm::Repo.generate_repo_configs
+    end
+  end
+
+  describe "#retrieve_repo_configs" do
+    it "fails if wget isn't available" do
+      Pkg::Util::Tool.stub(:find_tool).with("wget", {:required => true}) {false}
+      expect {Pkg::Rpm::Repo.generate_repo_configs}.to raise_error(RuntimeError)
+    end
+
+    it "fails if there are no deb repos available for the build" do
+      Pkg::Util::Tool.should_receive(:find_tool).with("wget", {:required => true}).and_return(wget)
+      FileUtils.should_receive(:mkdir_p).with("pkg/repo_configs").and_return(true)
+      Pkg::Util::Execution.should_receive(:ex).with("#{wget} -r -np -nH --cut-dirs 3 -P pkg/repo_configs --reject 'index*' #{base_url}/repo_configs/rpm/").and_raise(RuntimeError)
+      expect {Pkg::Rpm::Repo.retrieve_repo_configs}.to raise_error(RuntimeError, /Couldn't retrieve rpm yum repo configs/)
+    end
+  end
+
+  describe "#create_repos" do
+    let(:command) { "/usr/bin/make some repos" }
+    let(:target_directory) { "/tmp/dir/thing" }
+
+    it "fails without createrepo" do
+      Dir.should_receive(:chdir).with(target_directory).and_yield
+      Pkg::Util::Tool.should_receive(:find_tool).with('createrepo', :required => true).and_raise(RuntimeError)
+      expect { Pkg::Rpm::Repo.create_repos(target_directory) }.to raise_error(RuntimeError)
+    end
+
+    it "makes a repo in the target directory" do
+      Dir.should_receive(:chdir).with(target_directory).and_yield
+      Pkg::Util::Tool.should_receive(:find_tool).with('createrepo', :required => true).and_return(command)
+      Pkg::Rpm::Repo.should_receive(:repo_creation_command).with(command).and_return("run this thing")
+      Pkg::Util::Execution.should_receive(:ex).with("bash -c 'run this thing'")
+      Pkg::Rpm::Repo.create_repos(target_directory)
+    end
+  end
+
+  describe "#ship_repo_configs" do
+    it "fails if there are no repo configs to ship" do
+      Pkg::Util::File.should_receive(:empty_dir?).with("pkg/repo_configs/rpm").and_return(true)
+      expect { Pkg::Rpm::Repo.ship_repo_configs }.to raise_error(RuntimeError, /No repo configs have been generated!/)
+    end
+
+    it "ships repo configs to the build server" do
+      Pkg::Config.jenkins_repo_path = "/a/b/c/d"
+      Pkg::Config.project = "thing2"
+      Pkg::Config.ref = "abcd1234"
+      Pkg::Config.distribution_server = "a.host.that.wont.exist"
+      repo_dir = "#{Pkg::Config.jenkins_repo_path}/#{Pkg::Config.project}/#{Pkg::Config.ref}/repo_configs/rpm"
+      Pkg::Util::File.should_receive(:empty_dir?).with("pkg/repo_configs/rpm").and_return(false)
+      Pkg::Rpm::Repo.should_receive(:invoke_task).with("pl:fetch")
+      Pkg::Util::Net.should_receive(:remote_ssh_cmd).with(Pkg::Config.distribution_server, "mkdir -p #{repo_dir}")
+      Pkg::Rpm::Repo.should_receive(:retry_on_fail).with(:times => 3)
+      Pkg::Rpm::Repo.ship_repo_configs
+    end
+  end
+end

ca0b7ba576c53ed9ec79ad2c43f16df9d8ec4422
Eric Timmerman <eric.timmerman@puppetlabs.com>
2014-08-14 11:21:51 -0700

    (CPR-57) Fix style from recent rebase
    This commit fixes the style problems introduced since pull request being
    opened. There is an addition of space before curly brace and the removal
    of a redundant begin directly following the function definition.

--- lib/packaging/rpm/repo.rb
+++ lib/packaging/rpm/repo.rb
@@ -67,14 +67,14 @@ module Pkg::Rpm::Repo
       # repodata folders in them, and second that those same directories also
       # contain rpms
       #
-      repo_urls = Pkg::Util::Execution.ex("#{wget} --spider -r -l 5 --no-parent #{repo_base} 2>&1").split.uniq.reject{ |x| x =~ /\?|index/ }.select{ |x| x =~ /http:.*repodata\/$/ }
+      repo_urls = Pkg::Util::Execution.ex("#{wget} --spider -r -l 5 --no-parent #{repo_base} 2>&1").split.uniq.reject { |x| x =~ /\?|index/ }.select { |x| x =~ /http:.*repodata\/$/ }
 
       # RPMs will always exist at the same directory level as the repodata
       # folder, which means if we go up a level we should find rpms
       #
       yum_repos = []
-      repo_urls.map{ |x| x.chomp('repodata/') }.each do |url|
-        unless Pkg::Util::Execution.ex("#{wget} --spider -r -l 1 --no-parent #{url} 2>&1").split.uniq.reject{ |x| x =~ /\?|index/ }.select{ |x| x =~ /http:.*\.rpm$/ }.empty?
+      repo_urls.map { |x| x.chomp('repodata/') }.each do |url|
+        unless Pkg::Util::Execution.ex("#{wget} --spider -r -l 1 --no-parent #{url} 2>&1").split.uniq.reject { |x| x =~ /\?|index/ }.select { |x| x =~ /http:.*\.rpm$/ }.empty?
           yum_repos << url
         end
       end
@@ -116,45 +116,43 @@ module Pkg::Rpm::Repo
     end
 
     def create_repos_from_artifacts
-      begin
-        # Formulate our command string, which will just find directories with rpms
-        # and create and update repositories.
-        #
-        artifact_directory = File.join(Pkg::Config.jenkins_repo_path, Pkg::Config.project, Pkg::Config.ref)
+      # Formulate our command string, which will just find directories with rpms
+      # and create and update repositories.
+      #
+      artifact_directory = File.join(Pkg::Config.jenkins_repo_path, Pkg::Config.project, Pkg::Config.ref)
 
-        ##
-        # Test that the artifacts directory exists on the distribution server.
-        # This will give us some more helpful output.
-        #
-        cmd = 'echo "Checking for build artifacts. Will exit if not found." ; '
-        cmd << "[ -d #{artifact_directory}/artifacts ] || exit 1 ; "
+      ##
+      # Test that the artifacts directory exists on the distribution server.
+      # This will give us some more helpful output.
+      #
+      cmd = 'echo "Checking for build artifacts. Will exit if not found." ; '
+      cmd << "[ -d #{artifact_directory}/artifacts ] || exit 1 ; "
 
-        ##
-        # Enter the directory containing the build artifacts and create repos.
-        #
-        cmd << "pushd #{artifact_directory} ; "
-        cmd << 'echo "Checking for running repo creation. Will wait if detected." ; '
-        cmd << "while [ -f .lock ] ; do sleep 1 ; echo -n '.' ; done ; "
-        cmd << 'echo "Setting lock" ; '
-        cmd << "touch .lock ; "
-        cmd << "rsync -avxl artifacts/ repos/ ; pushd repos ; "
-        cmd << "createrepo=$(which createrepo) ; "
-        cmd << 'for repodir in $(find ./ -name "*.rpm" | xargs -I {} dirname {}) ; do '
-        cmd << "[ -d ${repodir} ] || continue; "
-        cmd << "pushd ${repodir} && ${createrepo} --checksum=sha --database --update . ; popd ; "
-        cmd << "done ; popd "
+      ##
+      # Enter the directory containing the build artifacts and create repos.
+      #
+      cmd << "pushd #{artifact_directory} ; "
+      cmd << 'echo "Checking for running repo creation. Will wait if detected." ; '
+      cmd << "while [ -f .lock ] ; do sleep 1 ; echo -n '.' ; done ; "
+      cmd << 'echo "Setting lock" ; '
+      cmd << "touch .lock ; "
+      cmd << "rsync -avxl artifacts/ repos/ ; pushd repos ; "
+      cmd << "createrepo=$(which createrepo) ; "
+      cmd << 'for repodir in $(find ./ -name "*.rpm" | xargs -I {} dirname {}) ; do '
+      cmd << "[ -d ${repodir} ] || continue; "
+      cmd << "pushd ${repodir} && ${createrepo} --checksum=sha --database --update . ; popd ; "
+      cmd << "done ; popd "
 
-        Pkg::Util::Net.remote_ssh_cmd(Pkg::Config.distribution_server, cmd)
-        # Now that we've created our repositories, we can create the configs for
-        # them
-        Pkg::Rpm::Repo.generate_repo_configs
+      Pkg::Util::Net.remote_ssh_cmd(Pkg::Config.distribution_server, cmd)
+      # Now that we've created our repositories, we can create the configs for
+      # them
+      Pkg::Rpm::Repo.generate_repo_configs
 
-        # And once they're created, we can ship them
-        Pkg::Rpm::Repo.ship_repo_configs
-      ensure
-        # Always remove the lock file, even if we've failed
-        Pkg::Util::Net.remote_ssh_cmd(Pkg::Config.distribution_server, "rm -f #{artifact_directory}/.lock")
-      end
+      # And once they're created, we can ship them
+      Pkg::Rpm::Repo.ship_repo_configs
+    ensure
+      # Always remove the lock file, even if we've failed
+      Pkg::Util::Net.remote_ssh_cmd(Pkg::Config.distribution_server, "rm -f #{artifact_directory}/.lock")
     end
   end
 end

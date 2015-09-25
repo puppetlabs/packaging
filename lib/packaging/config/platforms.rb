5fbda32a6095a7eb3abaa7cc7b86e0a0e4e563ad
Matthaus Owens <matthaus@puppetlabs.com>
2015-09-24 21:51:25 -0700

    (maint) Sort platforms by name
    Maintaining the platform hash will be easier if the platforms are sorted
    in some manner so this commit sorts them by name.

--- lib/packaging/config/platforms.rb
+++ lib/packaging/config/platforms.rb
@@ -1,17 +1,14 @@
 module Pkg::Platforms
   # Each element in this hash
   PLATFORM_INFO = {
-    'el' => {
-      '4' => { :architectures => ['i386', 'x86_64'], :repo => false, :package_format => 'rpm', :signature_format => 'v3', },
-      '5' => { :architectures => ['i386', 'x86_64'], :repo => true, :package_format => 'rpm', :signature_format => 'v3', },
-      '6' => { :architectures => ['i386', 'x86_64'], :repo => true, :package_format => 'rpm', :signature_format => 'v4', },
-      '7' => { :architectures => ['x86_64'], :repo => true, :package_format => 'rpm', :signature_format => 'v4', },
+    'aix' => {
+      '5.3' => { :architectures => ['power'], :repo => false, :package_format => 'rpm', },
+      '6.1' => { :architectures => ['power'], :repo => false, :package_format => 'rpm', },
+      '7.1' => { :architectures => ['power'], :repo => false, :package_format => 'rpm', },
     },
 
-    'sles' => {
-      '10' => { :architectures => ['i386', 'x86_64'], :repo => true, :package_format => 'rpm', :signature_format => 'v3', },
-      '11' => { :architectures => ['i386', 'x86_64'], :repo => true, :package_format => 'rpm', :signature_format => 'v3', },
-      '12' => { :architectures => ['x86_64'], :repo => true, :package_format => 'rpm', :signature_format => 'v4', },
+    'cumulus' => {
+      '2.2' => { :codename => 'cumulus', :architectures => ['amd64'], :repo => true, :package_format => 'deb', },
     },
 
     'debian' => {
@@ -21,35 +18,39 @@ module Pkg::Platforms
       '9' => { :codename => 'stretch', :architectures  => ['i386', 'amd64'], :repo => true, :package_format => 'deb', },
     },
 
-    'ubuntu' => {
-      '10.04' => { :codename => 'lucid', :architectures   => ['i386', 'amd64'], :repo => true, :package_format => 'deb', },
-      '12.04' => { :codename => 'precise', :architectures => ['i386', 'amd64'], :repo => true, :package_format => 'deb', },
-      '14.04' => { :codename => 'trusty', :architectures => ['i386', 'amd64'], :repo => true, :package_format => 'deb', },
-      '15.04' => { :codename => 'vivid', :architectures => ['i386', 'amd64'], :repo => true, :package_format => 'deb', },
-    },
-
-    'cumulus' => {
-      '2.2' => { :codename => 'cumulus', :architectures => ['amd64'], :repo => true, :package_format => 'deb', },
-    },
-
-    'aix' => {
-      '5.3' => { :architectures => ['power'], :repo => false, :package_format => 'rpm', },
-      '6.1' => { :architectures => ['power'], :repo => false, :package_format => 'rpm', },
-      '7.1' => { :architectures => ['power'], :repo => false, :package_format => 'rpm', },
-    },
-
-    'solaris' => {
-      '10' => { :architectures => ['i386', 'sparc'], :repo => false, :package_format => 'srv4', },
-      '11' => { :architectures => ['i386', 'sparc'], :repo => false, :package_format => 'ips', },
+    'el' => {
+      '4' => { :architectures => ['i386', 'x86_64'], :repo => false, :package_format => 'rpm', :signature_format => 'v3', },
+      '5' => { :architectures => ['i386', 'x86_64'], :repo => true, :package_format => 'rpm', :signature_format => 'v3', },
+      '6' => { :architectures => ['i386', 'x86_64'], :repo => true, :package_format => 'rpm', :signature_format => 'v4', },
+      '7' => { :architectures => ['x86_64'], :repo => true, :package_format => 'rpm', :signature_format => 'v4', },
     },
 
     'eos' => {
       '4' => { :architectures => ['i386'], :repo => false, :package_format => 'swix', },
     },
 
+    },
+
     'osx' => {
       '10.9' => { :architectures => ['x86_64'], :repo => false, :package_format => 'dmg', },
       '10.10' => { :architectures => ['x86_64'], :repo => false, :package_format => 'dmg', },
+    },
+
+    'sles' => {
+      '10' => { :architectures => ['i386', 'x86_64'], :repo => true, :package_format => 'rpm', :signature_format => 'v3', },
+      '11' => { :architectures => ['i386', 'x86_64'], :repo => true, :package_format => 'rpm', :signature_format => 'v3', },
+      '12' => { :architectures => ['x86_64'], :repo => true, :package_format => 'rpm', :signature_format => 'v4', },
+    },
+
+    'solaris' => {
+      '10' => { :architectures => ['i386', 'sparc'], :repo => false, :package_format => 'srv4', },
+      '11' => { :architectures => ['i386', 'sparc'], :repo => false, :package_format => 'ips', },
+
+    'ubuntu' => {
+      '10.04' => { :codename => 'lucid', :architectures   => ['i386', 'amd64'], :repo => true, :package_format => 'deb', },
+      '12.04' => { :codename => 'precise', :architectures => ['i386', 'amd64'], :repo => true, :package_format => 'deb', },
+      '14.04' => { :codename => 'trusty', :architectures => ['i386', 'amd64'], :repo => true, :package_format => 'deb', },
+      '15.04' => { :codename => 'vivid', :architectures => ['i386', 'amd64'], :repo => true, :package_format => 'deb', },
     },
 
     'windows' => {

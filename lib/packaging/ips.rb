d3aedf8b470d020f187baaa68e13ef8ee89742af
Morgan Haskel <morgan@puppetlabs.com>
2015-10-19 15:50:29 -0700

    (CPR-243) Add ability to sign for Solaris 11
    This will allow us to sign Solaris 11 packages from rake tasks in
    the packaging repo. Signing for Sol11 is a complicated process, so we
    rsync the files up to the signing server, create a new repo there, sign
    the repo, pull the packaging files back out, and rsync them back to our
    build box. This adds the Pkg::IPS library to be able to sign packages,
    and updates the rake task to use the new functionality.

---
+++ lib/packaging/ips.rb
@@ -0,0 +1,40 @@
+module Pkg::IPS
+  class << self
+    def sign(target_dir = 'pkg')
+      use_identity = "-i #{Pkg::Config.ips_signing_ssh_key}" unless Pkg::Config.ips_signing_key.nil?
+
+      ssh_host_string = "#{use_identity} #{ENV['USER']}@#{Pkg::Config.ips_signing_server}"
+      rsync_host_string = "-e 'ssh #{use_identity}' #{ENV['USER']}@#{Pkg::Config.ips_signing_server}"
+
+      p5ps = Dir.glob("#{target_dir}/solaris/11/**/*.p5p")
+
+      work_dir     = "/tmp/#{rand_string}"
+      unsigned_dir = "#{work_dir}/unsigned"
+      repo_dir     = "#{work_dir}/repo"
+      signed_dir   = "#{work_dir}/pkgs"
+
+      Pkg::Util::Net.remote_ssh_cmd(ssh_host_string, "mkdir -p #{repo_dir} #{unsigned_dir} #{signed_dir}")
+      Pkg::Util::Net.rsync_to(p5ps.join(" "), rsync_host_string, unsigned_dir)
+
+      # Before we can get started with signing packages we need to create a repo
+      Pkg::Util::Net.remote_ssh_cmd(ssh_host_string, "sudo -E /usr/bin/pkgrepo create #{repo_dir}")
+      Pkg::Util::Net.remote_ssh_cmd(ssh_host_string, "sudo -E /usr/bin/pkgrepo set -s #{repo_dir} publisher/prefix=puppetlabs.com")
+      # And import all the packages into the repo.
+      Pkg::Util::Net.remote_ssh_cmd(ssh_host_string, "sudo -E /usr/bin/pkgrecv -s #{unsigned_dir}/*.p5p -d #{repo_dir} '*'")
+      # We sign the entire repo
+      Pkg::Util::Net.remote_ssh_cmd(ssh_host_string, "sudo -E /usr/bin/pkgsign \
+                                          -c #{Pkg::Config.ips_signing_cert} \
+                                          -i #{Pkg::Config.ips_inter_cert} \
+                                          -i #{Pkg::Config.ips_root_cert} \
+                                          -k #{Pkg::Config.ips_signing_key} \
+                                          -s 'file://#{work_dir}/repo' '*'")
+      p5ps.each do |p5p|
+        # pkgrecv with -a will pull packages out of the repo, so we need to do that too to actually get the packages we signed
+        Pkg::Util::Net.remote_ssh_cmd(ssh_host_string, "sudo -E /usr/bin/pkgrecv -d #{signed_dir}/#{File.basename(p5p)} -a -s #{repo_dir} '*'")
+        # and pull the packages back.
+        Pkg::Util::Net.rsync_from("#{signed_dir}/#{File.basename(p5p)}", rsync_host_string, File.dirname(p5p))
+      end
+      Pkg::Util::Net.remote_ssh_cmd(ssh_host_string, "if [ -e '#{work_dir}' ] ; then sudo rm -r '#{work_dir}' ; fi")
+    end
+  end
+end

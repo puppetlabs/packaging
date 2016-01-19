e02ca929555ab79c6b631a84e35ff0492f82507a
Ryan McKern <ryan.mckern@puppetlabs.com>
2016-01-19 13:26:47 -0800

    (MAINT) Refactoring Pkg::Util::Net: remove fetch_uri
    Step 3: Start pruning unused methods. The best code is no code.
    Nothing except the test called 'fetch_uri', so 'fetch_uri' is
    first on the chopping block.

--- lib/packaging/util/net.rb
+++ lib/packaging/util/net.rb
@@ -1,21 +1,11 @@
 # Utility methods for handling network calls and interactions
 
-require 'open-uri'
 require 'open3'
 require 'socket'
 
 module Pkg
   module Util
     module Net
-      # This simple method does an HTTP get of a URI and writes it to a file
-      # in a slightly more platform agnostic way than curl/wget
-      def fetch_uri(uri, target)
-        if Pkg::Util::File.file_writable?(File.dirname(target))
-          File.open(target, 'w') { |f| f.puts(open(uri).read) }
-        end
-      end
-      module_function :fetch_uri
-
       # Get the hostname of the current host
       def hostname
         Socket.gethostname
--- spec/lib/packaging/util/net_spec.rb
+++ spec/lib/packaging/util/net_spec.rb
@@ -10,22 +10,6 @@ describe "Pkg::Util::Net" do
   let(:ssh)        { "/usr/local/bin/ssh" }
   let(:s3cmd)      { "/usr/local/bin/s3cmd" }
 
-  describe "#fetch_uri" do
-    context "given a target directory" do
-      it "does nothing if the directory isn't writable" do
-        File.stub(:writable?).with(File.dirname(target)) { false }
-        File.should_receive(:open).never
-        Pkg::Util::Net.fetch_uri(target_uri, target)
-      end
-
-      it "writes the content of the uri to a file if directory is writable" do
-        File.should_receive(:writable?).once.with(File.dirname(target)) { true }
-        File.should_receive(:open).once.with(target, 'w')
-        Pkg::Util::Net.fetch_uri(target_uri, target)
-      end
-    end
-  end
-
   describe "hostname utils" do
 
     describe "hostname" do

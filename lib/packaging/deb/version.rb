4a5e79c3079ed470a60088ad8b55da61466670a2
Melissa Stone <melissa@puppet.com>
2017-05-16 23:52:42 +0000

    this is probably a thing

--- lib/packaging/deb/version.rb
+++ lib/packaging/deb/version.rb
@@ -1,7 +1,5 @@
 module Pkg::Deb::Version
   class << self
-    attr_reader :debversion, :origversion
-
     def debversion
       Pkg::Util::Version.get_base_pkg_version.join('-') << "#{Pkg::Config.packager}1"
     end

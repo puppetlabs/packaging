c06e85a4abd16f3d05e310b278f0684989ce836b
Melissa Stone <melissa@puppet.com>
2017-05-19 01:25:21 +0000

    (maint) Remove el_version method
    This commit removes the `el_version` method. We only use this in a few
    isolated instances in old and unused code. Though this commit doesn't
    remove all the old and unused code, it does remove this bit. This is
    because this particular piece of code is the only bit in version.rb that
    really isn't needed.

--- lib/packaging/util/version.rb
+++ lib/packaging/util/version.rb
@@ -147,19 +147,6 @@ module Pkg::Util::Version
       return FALSE
     end
 
-    # Utility method to return the dist method if this is a redhat box. We use this
-    # in rpm packaging to define a dist macro, and we use it in the pl:fetch task
-    # to disable ssl checking for redhat 5 because it has a certs bundle so old by
-    # default that it's useless for our purposes.
-    def el_version
-      if File.exists?('/etc/fedora-release')
-        nil
-      elsif File.exists?('/etc/redhat-release')
-        rpm = Pkg::Util::Tool.find_tool('rpm', :required => true)
-        stdout, _, _ = Pkg::Util::Execution.capture3("#{rpm} -q --qf \"%{VERSION}\" $(#{rpm} -q --whatprovides /etc/redhat-release )")
-        stdout
-      end
-    end
 
     # This is to support packages that only burn-in the version number in the
     # release artifact, rather than storing it two (or more) times in the

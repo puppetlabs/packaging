1d8ef72b4f6e265c3b1d49adb17662112d412efd
Molly Waggett <molly.waggett@puppet.com>
2017-09-25 11:59:36 -0700

    (PA-1572) Add fedora-26-x86_64 as a supported platform

--- lib/packaging/platforms.rb
+++ lib/packaging/platforms.rb
@@ -38,8 +38,10 @@ module Pkg
       'fedora' => {
         'f24' => { :architectures => ['i386', 'x86_64'], :repo => true, :package_format => 'rpm', :signature_format => 'v4', },
         'f25' => { :architectures => ['i386', 'x86_64'], :repo => true, :package_format => 'rpm', :signature_format => 'v4', },
+        'f26' => { :architectures => ['x86_64'], :repo => true, :package_format => 'rpm', :signature_format => 'v4', },
         '24' => { :architectures => ['i386', 'x86_64'], :repo => true, :package_format => 'rpm', :signature_format => 'v4', },
         '25' => { :architectures => ['i386', 'x86_64'], :repo => true, :package_format => 'rpm', :signature_format => 'v4', },
+        '26' => { :architectures => ['x86_64'], :repo => true, :package_format => 'rpm', :signature_format => 'v4', },
       },
 
       'huaweios' => {

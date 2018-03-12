863b0057e9725048212a4f1a85991d90d5d78299
Molly Waggett <molly.waggett@puppet.com>
2018-03-12 11:03:15 -0700

    Revert "(maint) Remove extra 'artifacts' directory from foss_only_retrieve"
    This reverts commit 5e1ff73a9a0d1e03acdefa5b9e2269160b959066.

--- lib/packaging/retrieve.rb
+++ lib/packaging/retrieve.rb
@@ -48,14 +48,14 @@ module Pkg::Retrieve
     unless Pkg::Config.foss_platforms
       fail "FOSS_ONLY specified, but I don't know anything about FOSS_PLATFORMS. Retrieve cancelled."
     end
-    default_wget(local_target, "#{build_url}", { 'level' => 1 })
+    default_wget(local_target, "#{build_url}/artifacts/", { 'level' => 1 })
     yaml_path = File.join(local_target, "#{Pkg::Config.ref}.yaml")
     unless File.readable?(yaml_path)
       fail "Couldn't read #{Pkg::Config.ref}.yaml, which is necessary for FOSS_ONLY. Retrieve cancelled."
     end
     platform_data = Pkg::Util::Serialization.load_yaml(yaml_path)[:platform_data]
     platform_data.each do |platform, paths|
-      default_wget(local_target, "#{build_url}/#{paths[:artifact]}") if Pkg::Config.foss_platforms.include?(platform)
+      default_wget(local_target, "#{build_url}/artifacts/#{paths[:artifact]}") if Pkg::Config.foss_platforms.include?(platform)
     end
   end
 

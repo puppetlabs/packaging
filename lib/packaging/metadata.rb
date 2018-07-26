74b73e8a9d9357b0e51f8efea7a7c931b1b8e258
Molly Waggett <molly.waggett@puppet.com>
2018-07-26 15:46:18 -0700

    (maint) Add more debug output in metadata retrieval methods

--- lib/packaging/metadata.rb
+++ lib/packaging/metadata.rb
@@ -4,6 +4,7 @@ module Pkg
     module_function
 
     def retrieve_metadata_section(section, catalog = nil)
+      puts "Beginning to attempt #{section} metadata retrieval . . ."
       artifactory = RelengMetadata::Artifactory.new
       catalog ||= artifactory.most_recent_catalog
       puts "Retrieving #{section} metadata from catalog #{catalog} . . ."
@@ -11,6 +12,7 @@ module Pkg
     end
 
     def retrieve_project_metadata(project, catalog = nil)
+      puts "Beginning to attempt #{project} metadata retrieval . . ."
       artifactory = RelengMetadata::Artifactory.new
       catalog ||= artifactory.most_recent_catalog
       puts "Retrieving #{project} metadata from catalog #{catalog} . . ."

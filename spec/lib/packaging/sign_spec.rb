7d48861fd84d704f0c8ca52eec38ab4caf497c42
Molly Waggett <molly.waggett@puppet.com>
2018-09-05 17:14:51 -0700

    (maint) Add signature responses from el5 and sles12 to `has_sig?` tests

--- spec/lib/packaging/sign_spec.rb
+++ spec/lib/packaging/sign_spec.rb
@@ -10,11 +10,25 @@ describe 'Pkg::Sign' do
 
     describe '#has_sig?' do
       let(:rpm) { 'foo.rpm' }
-      let(:signed_response) { <<-DOC
+      let(:el7_signed_response) { <<-DOC
 Header V4 RSA/SHA256 Signature, key ID ef8d349f: NOKEY
 Header SHA1 digest: OK (3cb7e9861e8bc09783a1b6c8d88243a3c16daa81)
 V4 RSA/SHA256 Signature, key ID ef8d349f: NOKEY
 MD5 digest: OK (d5f06ba2a9053de532326d0659ec0d11)
+DOC
+      }
+      let(:el5_signed_response) { <<-DOC
+Header V3 RSA/SHA1 signature: NOKEY, key ID ef8d349f
+Header SHA1 digest: OK (12ea7bd578097a3aecc5deb8ada6aca6147d68e3)
+V3 RSA/SHA1 signature: NOKEY, key ID ef8d349f
+MD5 digest: OK (27353c6153068a3c9902fcb4ad5b8b92)
+DOC
+      }
+      let(:sles12_signed_response) { <<-DOC
+Header V4 RSA/SHA256 Signature, key ID ef8d349f: NOKEY
+Header SHA1 digest: OK (e713487cf21ebeb933aefd5ec9211a34603233d2)
+V4 RSA/SHA256 Signature, key ID ef8d349f: NOKEY
+MD5 digest: OK (3093a09ac39bc17751f913e19ca74432)
 DOC
       }
       let(:unsigned_response) { <<-DOC
@@ -22,8 +36,16 @@ Header SHA1 digest: OK (f9404cc95f200568c2dbb1fd24e1119e3e4a40a9)
 MD5 digest: OK (816095f3cee145091c3fa07a0915ce85)
 DOC
       }
-      it 'returns true if rpm has been signed' do
-        allow(Pkg::Util::Execution).to receive(:capture3).and_return([signed_response, '', 0])
+      it 'returns true if rpm has been signed (el7)' do
+        allow(Pkg::Util::Execution).to receive(:capture3).and_return([el7_signed_response, '', 0])
+        expect(Pkg::Sign::Rpm.has_sig?(rpm)).to be true
+      end
+      it 'returns true if rpm has been signed (el5)' do
+        allow(Pkg::Util::Execution).to receive(:capture3).and_return([el5_signed_response, '', 0])
+        expect(Pkg::Sign::Rpm.has_sig?(rpm)).to be true
+      end
+      it 'returns true if rpm has been signed (sles12)' do
+        allow(Pkg::Util::Execution).to receive(:capture3).and_return([sles12_signed_response, '', 0])
         expect(Pkg::Sign::Rpm.has_sig?(rpm)).to be true
       end
       it 'returns false if rpm has not been signed' do

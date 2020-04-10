269a24a9789a730c224327efbc350cd5c7bbc3e5
Eric Griswold <eric.griswold@puppet.com>
2020-04-10 10:34:06 -0700

    (RE-13273) Remove obsolete handling of PC1 repos

--- spec/lib/packaging/paths_spec.rb
+++ spec/lib/packaging/paths_spec.rb
@@ -211,32 +211,6 @@ describe 'Pkg::Paths' do
     end
   end
 
-  describe '#is_legacy_repo?' do
-    it 'returns true for empty strings' do
-      expect(Pkg::Paths.is_legacy_repo?('')).to be_true
-    end
-
-    it 'returns true for PC1' do
-      expect(Pkg::Paths.is_legacy_repo?('PC1')).to be_true
-    end
-
-    it 'returns true for foopuppetbar' do
-      expect(Pkg::Paths.is_legacy_repo?('foopuppetbar')).to be_true
-    end
-
-    it 'returns false for puppet5' do
-      expect(Pkg::Paths.is_legacy_repo?('puppet5')).to be_false
-    end
-
-    it 'returns false for puppet8-nightly' do
-      expect(Pkg::Paths.is_legacy_repo?('puppet8-nightly')).to be_false
-    end
-
-    it 'returns false for puppet' do
-      expect(Pkg::Paths.is_legacy_repo?('puppet')).to be_false
-    end
-  end
-
   describe '#remote_repo_base' do
     before :each do
       allow(Pkg::Config).to receive(:yum_repo_path).and_return('foo')

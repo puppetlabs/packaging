048512aebc2bbb2db42aee1cb44368a6e9c273a3
isaac-hammes <isaac.hammes@puppet.com>
2022-04-26 10:14:51 -0700

    (RE-13764) Fix order to prioritize environment variable over default value for rvm version in remote_bundle_install

--- lib/packaging/util/net.rb
+++ lib/packaging/util/net.rb
@@ -394,7 +394,7 @@ module Pkg::Util::Net
     end
 
     def remote_bundle_install_command
-      rvm_ruby_version = '2.5.9' || ENV['RVM_RUBY_VERSION']
+      rvm_ruby_version = ENV['RVM_RUBY_VERSION'] || '2.5.9'
       export_packaging_location = "export PACKAGING_LOCATION='#{ENV['PACKAGING_LOCATION']}';" if ENV['PACKAGING_LOCATION'] && !ENV['PACKAGING_LOCATION'].empty?
       export_vanagon_location = "export VANAGON_LOCATION='#{ENV['VANAGON_LOCATION']}';" if ENV['VANAGON_LOCATION'] && !ENV['VANAGON_LOCATION'].empty?
       "source /usr/local/rvm/scripts/rvm; rvm use ruby-#{rvm_ruby_version}; #{export_packaging_location} #{export_vanagon_location} bundle install --path .bundle/gems ;"

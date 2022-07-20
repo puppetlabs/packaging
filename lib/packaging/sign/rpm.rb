2732527290d01d2be54c4a876ca0a8c472ae61c5
Morgan Rhodes <morgan@puppet.com>
2022-07-20 09:22:44 -0700

    no tty?

--- lib/packaging/sign/rpm.rb
+++ lib/packaging/sign/rpm.rb
@@ -9,7 +9,7 @@ module Pkg::Sign::Rpm
 
     # on gpg >= 2.1 you need to specify the pinentry mode and not specify the
     # batch option to get prompted for a passphrase
-    input_flag = "--pinentry-mode loopback"
+    input_flag = "--pinentry-mode loopback --no-tty"
     gpg_check_command = ''
     gpg_legacy_hosts = Pkg::Config.gpg_legacy_hosts || []
 
